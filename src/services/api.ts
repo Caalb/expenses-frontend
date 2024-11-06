fed864f78f5ed1eba38c7092fb7dcdcf4affe89b
Caalb <carlosmunizfjunior@gmail.com>
2024-11-06 15:59:05 -0300

    feat: adiciona interceptores de requisição e resposta para gerenciamento de autenticação

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,7 @@
-import axios from 'axios'
+import { JWT_TOKEN } from '@/constants/jwt'
+import axios, { AxiosError } from 'axios'
+import { useQuasar } from 'quasar'
+import { useRouter } from 'vue-router'
 
 const api = axios.create({
   baseURL: 'http://localhost:3000',
@@ -6,5 +9,33 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(JWT_TOKEN)
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      const router = useRouter()
+      console.log(error)
+
+      console.log(router.push({ name: 'login' }))
+      router.push({ name: 'login' })
+    }
+
+    return Promise.reject(error)
+  },
+)
 
 export default api
