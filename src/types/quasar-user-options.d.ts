8a6dd7de385f79fcb7965b3102507133ebe188da
Caalb <carlosmunizfjunior@gmail.com>
2024-11-06 01:23:21 -0300

    feat: remove definição de tipo para opções de usuário do Quasar

--- src/types/quasar-user-options.d.ts
+++
@@ -1,7 +0,0 @@
-declare module './quasar-user-options' {
-  const quasarUserOptions: {
-    config: Record<string, unknown>
-    plugins: Record<string, unknown>
-  }
-  export default quasarUserOptions
-}
