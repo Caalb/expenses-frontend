c401d2208f48b7d4316040b20fcef75264576f81
Caalb <carlosmunizfjunior@gmail.com>
2024-11-07 08:06:42 -0300

    fix: atualiza URL base da API para usar variável de ambiente VITE_API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { Notify } from 'quasar'
 import { JWT_TOKEN } from '@/constants/jwt'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
