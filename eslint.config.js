06930661b3a0bb0d70ca39b6893430a44c8c919a
Caalb <carlosmunizfjunior@gmail.com>
2024-11-07 11:30:00 -0300

    feat: adiciona regras de linting para Vue no eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -20,4 +20,33 @@ export default [
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
+
+  {
+    rules: {
+      'vue/html-closing-bracket-newline': [
+        'error',
+        {
+          singleline: 'never',
+          multiline: 'always',
+        },
+      ],
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 1,
+          multiline: 1,
+        },
+      ],
+      'vue/html-indent': [
+        'error',
+        2,
+        {
+          attribute: 1,
+          baseIndent: 1,
+          closeBracket: 0,
+          alignAttributesVertically: true,
+        },
+      ],
+    },
+  },
 ]
